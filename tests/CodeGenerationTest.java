92e2abff613ef64443962635c54c26b499e22808
Jacob Søndergaard <jacob@cavas.dk>
2023-05-16 13:49:35 +0200

    Fix CG test

--- tests/CodeGenerationTest.java
+++ tests/CodeGenerationTest.java
@@ -1,21 +1,11 @@
-import com.eel.AST.BuildASTVisitor;
 import com.eel.AST.nodes.FormalParametersNode;
 import com.eel.AST.nodes.ProcedureDeclarationNode;
 import com.eel.AST.nodes.ProcedureNode;
 import com.eel.AST.nodes.ProgramNode;
 import com.eel.Generator;
-import com.eel.antlr.eelLexer;
-import com.eel.antlr.eelParser;
-import org.antlr.v4.runtime.CharStreams;
-import org.antlr.v4.runtime.CommonTokenStream;
-import org.antlr.v4.runtime.tree.ParseTree;
 import org.antlr.v4.runtime.tree.TerminalNode;
 import org.antlr.v4.runtime.tree.TerminalNodeImpl;
 import org.junit.Test;
-
-import java.io.IOException;
-import java.nio.file.*;
-
 import java.util.ArrayList;
 
 public class CodeGenerationTest {
@@ -26,7 +16,6 @@ public class CodeGenerationTest {
         ProcedureDeclarationNode procedureDeclarationNode = new ProcedureDeclarationNode(0,0, terminalNode, formalParametersNode);
 
         ProcedureNode procedureNode = new ProcedureNode(0,0, procedureDeclarationNode, new ArrayList<>());
-        ProcedureNode procedureNode2 = new ProcedureNode(0,0, procedureDeclarationNode, new ArrayList<>());
 
         ArrayList<ProcedureNode> procedureNodes = new ArrayList<>();
         procedureNodes.add(procedureNode);
